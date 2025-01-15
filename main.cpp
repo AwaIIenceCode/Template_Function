6de992fe272e9cbe365f113926eec2c568316f35
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2025-01-15 15:57:48 +0200

    ex_2 is done

--- main.cpp
+++ main.cpp
@@ -1,41 +1,50 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 template <typename T>
-void sortArray(T arr[], int size, bool descending = false)
+void insertionSort(T arr[], int start, int end, bool descending)
 {
-    if (descending)
+    for (int i = start + 1; i <= end; i++)
     {
-        sort(arr, arr + size, greater<T>());
+        T key = arr[i];
+        int j = i - 1;
+
+        while (j >= start && ((descending && arr[j] < key) || (!descending && arr[j] > key)))
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
     }
-    else
-    {
-        sort(arr, arr + size);
-    }
+}
+
+template <typename T>
+void sortHalfArray(T arr[], int size)
+{
+    int mid = size / 2;
+
+    insertionSort(arr, 0, mid - 1, true);
+    insertionSort(arr, mid, size - 1, false);
 }
 
 int main()
 {
-    int arr[10] = {5, 1, 8, 3, 9, 2, 7, 4, 6, 0};
+    int arr[10] = {9, 7, 3, 1, 6, 5, 4, 2, 8, 0};
 
     cout << "Before sorting:\n";
+
     for (int i = 0; i < 10; i++)
-    {
         cout << arr[i] << " ";
-    }
 
     cout << endl;
 
-    sortArray(arr, 10, true);
+    sortHalfArray(arr, 10);
 
     cout << "After sorting:\n";
-
     for (int i = 0; i < 10; i++)
-    {
         cout << arr[i] << " ";
-    }
 
     cout << endl;
 
